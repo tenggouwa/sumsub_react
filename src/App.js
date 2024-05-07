250d6d1d757328c284a7a49ae87a1c1c14759b64
tenggouwa <17681875268@163.com>
2024-05-07 17:12:07 +0800

    feat: changed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import SumsubWebSdk from "@sumsub/websdk-react";
 import './App.css';
 
 function App() {
+  const [token, setToken] = useState('_act-sbx-jwt-eyJhbGciOiJub25lIn0.eyJqdGkiOiJfYWN0LXNieC0zOGU3MWZkMS00NDIzLTRkNzEtYTI5Ni1kMmUwYzI4N2RhYzctdjIiLCJ1cmwiOiJodHRwczovL2FwaS5zdW1zdWIuY29tIn0.-v2')
+  useEffect(() => {
+    getToken()
+  }, [])
+
+  const applicantEmail = "";
+  const applicantPhone = "";
+  const getToken = async () => {
+    const res = await axios.get('http://8.149.134.150/api/public/kyc/access-token?userId=1');
+    if (res?.data?.success) {
+      setToken(res?.data?.data?.token)
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SumsubWebSdk
+        accessToken={token}
+        updateAccessToken={() => console.log("updateAccessToken")}
+        expirationHandler={() => Promise.resolve(token)}
+        config={{
+          lang: "zh-tw",
+          email: applicantEmail,
+          phone: applicantPhone,
+          i18n: {
+            document: {
+              subTitles: {
+                IDENTITY: "Upload a document that proves your identity"
+              }
+            }
+          },
+          onMessage: (type, payload) => {
+            console.log("WebSDK onMessage", type, payload);
+          },
+          uiConf: {
+            customCssStr:
+              ":root {\n  --black: #000000;\n   --grey: #F5F5F5;\n  --grey-darker: #B2B2B2;\n  --border-color: #DBDBDB;\n}\n\np {\n  color: var(--black);\n  font-size: 16px;\n  line-height: 24px;\n}\n\nsection {\n  margin: 40px auto;\n}\n\ninput {\n  color: var(--black);\n  font-weight: 600;\n  outline: none;\n}\n\nsection.content {\n  background-color: var(--grey);\n  color: var(--black);\n  padding: 40px 40px 16px;\n  box-shadow: none;\n  border-radius: 6px;\n}\n\nbutton.submit,\nbutton.back {\n  text-transform: capitalize;\n  border-radius: 6px;\n  height: 48px;\n  padding: 0 30px;\n  font-size: 16px;\n  background-image: none !important;\n  transform: none !important;\n  box-shadow: none !important;\n  transition: all 0.2s linear;\n}\n\nbutton.submit {\n  min-width: 132px;\n  background: none;\n  background-color: var(--black);\n}\n\n.round-icon {\n  background-color: var(--black) !important;\n  background-image: none !important;\n}"
+          },
+          onError: (error) => {
+            console.error("WebSDK onError", error);
+          }
+        }}
+        options={{ addViewportTag: false, adaptIframeHeight: true }}
+        onMessage={(type, payload) => {
+          console.log("onMessage", type, payload);
+        }}
+        onError={(data) => console.log("onError", data)}
+      />
     </div>
   );
 }
